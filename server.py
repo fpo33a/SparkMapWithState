5ce573476137ed32bc0ac0f64f12788cc11e733c
Frank Polet <frank.polet@gmail.com>
2019-08-25 13:33:53 +0200

    initial commit

---
+++ server.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3           # This is server.py file
+import socket
+import time
+
+# create a socket object
+serversocket = socket.socket(
+	        socket.AF_INET, socket.SOCK_STREAM)
+
+# get local machine name
+host = "localhost"
+
+port = 9999
+
+# bind to the port
+serversocket.bind((host, port))
+
+# queue up to 5 requests
+serversocket.listen(5)
+
+while True:
+   # establish a connection
+   clientsocket,addr = serversocket.accept()
+
+   print("Got a connection from %s" % str(addr))
+
+   msg = 'Thank you for connecting frank. This is the connecting for test. This is hello'+ "\r\n"  +' hello frank'  + "\r\n"  +' hello jean'  + "\r\n"
+   clientsocket.send(msg.encode('ascii'))
+
+   time.sleep (10)
+   msg = 'this is another batch '+ "\r\n"  +' Thank you frank'  + "\r\n"  +' and jean'  + "\r\n"
+   clientsocket.send(msg.encode('ascii'))
+
+   time.sleep (15)
+
+   msg = 'this is roro and riri\r\n'
+   clientsocket.send(msg.encode('ascii'))
+
+#   clientsocket.close()
